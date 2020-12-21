485360c0aed11bdf83a9b91bb1f28d6f680dcee8
Andrew Gosselin <gosseand2019@gmail.com>
2020-12-21 14:53:46 -0500

    Initial commit

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.dash')
+
+@section('styles')
+<style>
+
+</style>
+@endsection
+
+@section('content')
+<h1 class="mt-4">Dashboard</h1>
+@endsection
+
+@section('scripts')
+<script>
+
+</script>
+@endsection
